0cd968574008b0544c3cb7efcfe99c830f776504
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-19 01:26:36 +0000

    Update BackButton.js as 'onPress' property did not match in other words it was inconsistent

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -3,20 +3,20 @@ import { Pressable, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "../theme/colors";
 
-export default function BackButton({ onpress }) {
-    return (
-        <Pressable onPress={onPress} style={({pressed}) => [StyleSheet.btn, pressed && { opacity: 0.8 }]}>
-            <Ionicons name="chevron-back" size={26} color={COLORS.white} />
-        </Pressable>
-    );
+export default function BackButton({ onPress }) {
+  return (
+    <Pressable onPress={onPress} style={({ pressed }) => [styles.btn, pressed && { opacity: 0.8 }]}>
+      <Ionicons name="chevron-back" size={26} color={COLORS.white} />
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    btn: {
-        width: 44,
-        height: 44,
-        borderRadius: 22,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  btn: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
