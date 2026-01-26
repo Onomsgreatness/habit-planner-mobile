19b2176f7c5982b05e39de30c4b647cb6e64c9f4
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-26 23:33:24 +0000

    Update App.js, as Expo-Go request permission once app is start

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigation/RootNavigator";
+import { ensureNotificationPermission } from "./src/services/notifications";
 
 export default function App() {
+    useEffect(() => {
+        ensureNotificationPermission();
+    }, []);
+
   return (
     <NavigationContainer>
       <RootNavigator />
